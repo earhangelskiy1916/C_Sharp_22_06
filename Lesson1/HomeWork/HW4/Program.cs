4c3a7c1807dca9a0f87f4227980859fe10ed5271
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-06-24 23:47:36 +0300

    feat: add HW4

---
+++ Lesson1/HomeWork/HW4/Program.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.WriteLine("write nubmer");
+string numberA = Console.ReadLine()!;
+int numA = int.Parse(numberA);
+int N = 0;
+while(N<numA)
+{
+    N=N+1;
+    if(N%2==0)
+    {
+        Console.WriteLine(N);
+    }
+    
+}
+
