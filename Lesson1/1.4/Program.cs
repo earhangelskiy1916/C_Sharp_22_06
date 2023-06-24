ebdecda0d6ffc04b0ccce228c44909cbecbad82a
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-06-24 17:41:56 +0300

    feat: add task 4

---
+++ Lesson1/1.4/Program.cs
@@ -0,0 +1,9 @@
+﻿// 4. Напишите программу вычисления модуля числа.
+Console.WriteLine("write number");
+string number = Console.ReadLine()!;
+int absolute = int.Parse(number);
+if(absolute<0)
+{
+    absolute = absolute*-1;
+}
+Console.WriteLine(absolute);
