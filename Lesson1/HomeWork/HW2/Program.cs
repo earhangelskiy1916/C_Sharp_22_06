068cc981924c57f808f6a5535fee46f276de427e
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-06-24 22:24:38 +0300

    feat: add HW2

---
+++ Lesson1/HomeWork/HW2/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("write three nubmers");
+string numberA = Console.ReadLine()!;
+string numberB = Console.ReadLine()!;
+string numberC = Console.ReadLine()!;
+int numA = int.Parse(numberA);
+int numB = int.Parse(numberB);
+int numC = int.Parse(numberC);
+int Max = numA;
+if(numB>Max)
+{
+    Max = numB;
+}
+if(numC>Max)
+{
+    Max = numC;
+}
+Console.WriteLine(Max);
