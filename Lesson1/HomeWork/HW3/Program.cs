376dd612066eef8cc31dc5ba7f29c84095679353
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-06-24 22:46:51 +0300

    feat: add HW3

---
+++ Lesson1/HomeWork/HW3/Program.cs
@@ -0,0 +1,15 @@
+﻿//Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+//число%2, если 0 -четное, если 1 - нечетное
+
+Console.WriteLine("write nubmer");
+string numberA = Console.ReadLine()!;
+int numA = int.Parse(numberA);
+if(numA%2 == 0)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
