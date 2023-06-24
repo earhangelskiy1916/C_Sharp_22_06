c9646741a506bb004eac0eafef30c95fd3a5ced4
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-06-24 17:05:14 +0300

    feat: add task3

---
+++ Lesson1/1.3/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+Console.WriteLine("write number");
+string number = Console.ReadLine()!;
+int num = int.Parse(number);
+int count = 0;
+if (num>0)
+{
+    while (count!=num*2+1)
+    {
+        
+        Console.Write(count-num);
+        count = count+1;
+    }
+}    
+else
+{
+    while (count!=num*2-1)
+    {
+        Console.Write(count-num);
+        count = count-1;
+    }
+
+}
+    
+
