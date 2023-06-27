a60a5cb1a733c378eccd6881a72c09bba59abf3e
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-06-27 20:01:52 +0300

    feat: add task1 for lesson 2

---
+++ Lesson2/2.0/Program.cs
@@ -0,0 +1,11 @@
+﻿int LastNumber (int Number)
+{
+    int NewNumber = 0;
+    NewNumber = Number%10;
+    Console.WriteLine(NewNumber);
+    return NewNumber;
+}
+
+int MyNumber = new Random().Next(100,1000);
+Console.WriteLine(MyNumber);
+LastNumber(MyNumber);
