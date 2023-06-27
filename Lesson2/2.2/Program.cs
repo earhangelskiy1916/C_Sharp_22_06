48e241476817aad9c24a147f75edfe832bcae0f1
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-06-27 21:38:56 +0300

    feat: add Task3 for Lesson2

---
+++ Lesson2/2.2/Program.cs
@@ -0,0 +1,30 @@
+﻿int Mult( int Num1, int Num2)
+{
+    int Result = 0;
+    
+    if(Num1%Num2 == 0 )
+    {
+        Console.Write(Num1);
+        Console.Write(",");
+        Console.Write(Num2);
+        Console.Write(",");
+        Console.Write("Yes");
+    }
+    else 
+    {
+        Result = Num1%Num2;
+        Console.Write(Num1);
+        Console.Write(",");
+        Console.Write(Num2); 
+        Console.Write(",");
+        Console.Write("No, remnant=");
+        Console.Write(Result);
+    }
+    return Result;
+}
+
+Console.WriteLine("Enter two numbers");
+int MyNumber1 = int.Parse(Console.ReadLine());
+int MyNumber2 = int.Parse(Console.ReadLine());
+Mult(MyNumber1, MyNumber2);
+
