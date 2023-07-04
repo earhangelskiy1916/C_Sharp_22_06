6055d247f545988c2cf32bb995dca4ef5af87e5e
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-07-04 22:50:53 +0300

    feat: add demo task

---
+++ Lesson4/4.0/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("write number");
+int Number = int.Parse(Console.ReadLine()!);
+int LastDigit = 0;
+int Sum(int AnyNumber)
+{
+    int sum = 0;
+    for (int i = 0; Number>0; i++)
+    {
+        LastDigit = Number%10;
+        sum = sum+LastDigit;
+        Number = Number-1;
+    }
+    return sum;
+}
+Console.WriteLine(Sum(Number));
