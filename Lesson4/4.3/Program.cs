3135860b8c8dc6cc52bbbcb74e7328c2336c3b3b
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-07-05 17:30:09 +0300

    feat: add task3

---
+++ Lesson4/4.3/Program.cs
@@ -0,0 +1,24 @@
+﻿
+// int[] NewArray = new int [8];
+// (new Random().Next(1,10));
+
+void Array (int[] NewArray)
+{
+    for (int i = 0; i < NewArray.Length; i++)
+    {
+        NewArray[i] = new Random().Next(0,2);
+    }
+}
+
+void List (int[] NewList)
+{
+    for (int j = 0; j < NewList.Length; j++)
+    {
+        Console.Write(NewList[j] + " ");
+    }
+}
+int[] MyArray = new int[8];
+
+Array(MyArray);
+List(MyArray);
+
