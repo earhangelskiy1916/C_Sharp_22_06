3418618c6d307934620471217be2aaf95e67d549
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-07-05 17:35:37 +0300

    feat: add task1

---
+++ Lesson4/HomeWork/1.1/Program.cs
@@ -0,0 +1,16 @@
+﻿void PrintA (int num1, int num2)
+{
+    int num3=num1;
+    for (int i = 1; i < num2; i++)
+    {
+        num1 = num1*num3;
+    }
+    Console.WriteLine(num1);
+}
+
+Console.WriteLine("Enter two numbers");
+int X = int.Parse(Console.ReadLine()!);
+int Y = int.Parse(Console.ReadLine()!);
+
+
+PrintA(X,Y);
