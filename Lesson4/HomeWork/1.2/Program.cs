39289d15eaa36625237680cee0d3a55f2f1509f8
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-07-05 17:49:29 +0300

    feat: add task 2

---
+++ Lesson4/HomeWork/1.2/Program.cs
@@ -0,0 +1,14 @@
+﻿void Sum(int Number)
+{
+    int sum = 0;
+    for (int i = 0; Number > 0; i++)
+    {
+        int LastNumber = Number % 10;
+        sum = sum + LastNumber;
+        Number = Number / 10;
+    }
+    Console.WriteLine(sum);
+}
+Console.WriteLine("write number");
+int Value = int.Parse(Console.ReadLine()!);
+Sum(Value);
