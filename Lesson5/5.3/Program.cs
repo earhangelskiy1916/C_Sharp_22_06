b587553aa60f2131675e12481826ad51ecfcf462
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-07-10 17:10:44 +0300

    feat: add task3

---
+++ Lesson5/5.3/Program.cs
@@ -0,0 +1,24 @@
+﻿int FillArray(int[] Array)
+{
+    Console.WriteLine("enter start range");
+    int StartRange = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("enter stop range");
+    int StopRange = int.Parse(Console.ReadLine()!);
+    int count = 0;
+
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = new Random().Next(StartRange, StopRange);
+        if (Array[i] > 9 && Array[i] < 100)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine("Enter length");
+int length = int.Parse(Console.ReadLine()!);
+int[] NewArray = new int[length];
+
+int quantity = FillArray(NewArray);
+Console.WriteLine(quantity);
