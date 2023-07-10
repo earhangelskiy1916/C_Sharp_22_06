cf4c4344b9561f15944bb9de4d8c365683109cfc
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-07-10 17:34:18 +0300

    feat: add HW1

---
+++ Lesson5/HomeWork/5.2/Program.cs
@@ -0,0 +1,26 @@
+﻿int FillArray(int[] Array)
+{
+    Console.WriteLine("enter start range");
+    int StartRange = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("enter stop range");
+    int StopRange = int.Parse(Console.ReadLine()!);
+    int count = 0;
+
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = new Random().Next(StartRange, StopRange);
+        if (i % 2 == 0)
+        {
+            count = count + Array[i];
+        }
+    }
+    Console.WriteLine(string.Join(",", Array));
+
+    return count;
+}
+
+Console.WriteLine("enter length");
+int length = int.Parse(Console.ReadLine()!);
+int[] MyArray = new int[length];
+int target = FillArray(MyArray);
+Console.WriteLine(target);
