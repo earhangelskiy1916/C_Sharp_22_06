7650ff20ea3e2b10d368443e5ce977c2a1a25de1
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-07-10 16:57:33 +0300

    feat: add demo task

---
+++ Lesson5/5.0/Program.cs
@@ -0,0 +1,31 @@
+﻿void FillArray(int[] Array)
+{
+    int pos = 0;
+    int neg = 0;
+    Console.WriteLine("enter start range");
+    int StartRange = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("enter stop range");
+
+    int StopRange = int.Parse(Console.ReadLine()!);
+
+    for (int i = 0; i < Array.Length; i++)
+    {
+        Array[i] = new Random().Next(StartRange, StopRange);
+        if (Array[i] < 0)
+        {
+            neg = neg + Array[i];
+        }
+        if (Array[i] > 0)
+        {
+            pos = pos + Array[i];
+        }
+    }
+    Console.WriteLine(pos);
+    Console.WriteLine(neg);
+}
+
+Console.WriteLine("array length is");
+int length = int.Parse(Console.ReadLine()!);
+int[] UserArray = new int[length];
+
+FillArray(UserArray);
