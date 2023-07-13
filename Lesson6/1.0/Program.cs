9763d647485d7fb7f5077e9e5eb048e0b13093c8
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-07-13 17:28:42 +0300

    add task 1

---
+++ Lesson6/1.0/Program.cs
@@ -0,0 +1,20 @@
+﻿
+void triangle(int a, int b , int c)
+{
+    if (a+b>c&& a+c>b&& b+c>a)
+    {
+        Console.WriteLine("yes");
+    }
+    else
+    {
+        Console.WriteLine("no");
+    }
+}
+
+Console.WriteLine("enter three numbers");
+int First = int.Parse(Console.ReadLine()!);
+int Second = int.Parse(Console.ReadLine()!);
+int Third = int.Parse(Console.ReadLine()!);
+
+triangle(First,Second,Third);
+
