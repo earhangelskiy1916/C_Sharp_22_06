3773a5ba016ce18aa8c64eb3232c5cb302b410a8
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-07-13 17:47:46 +0300

    feat:add tsk 3

---
+++ Lesson6/1.2/Program.cs
@@ -0,0 +1,17 @@
+﻿void Fib(int a)
+{
+    int temp1 = 0;
+    int temp2 = 1;
+    
+    for (int i = 0; i < a; i++)
+    {
+        Console.WriteLine(temp1);
+        (temp1, temp2) = (temp2, temp2+temp1);
+    }
+    
+}
+
+Console.WriteLine("enter number");
+int First = int.Parse(Console.ReadLine()!);
+
+Fib(First);
