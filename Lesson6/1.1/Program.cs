e55bf2b497a8691f1d9d74cf47066fe1886b4799
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-07-13 17:40:40 +0300

    feat task2

---
+++ Lesson6/1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿string Bin(int a)
+{
+    string number = string.Empty;
+    for(; a!=0; a/=2)
+    {
+        number = a%2+number;
+    }
+    return number;
+}
+
+Console.WriteLine("enter number");
+int First = int.Parse(Console.ReadLine()!);
+
+string BinResult =Bin(First);
+Console.Write(BinResult);
