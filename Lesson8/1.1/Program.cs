641891b9498c0c39b7ac2c465f3eade67388e1e4
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-07-18 13:52:14 +0300

    feat: add task 1

---
+++ Lesson8/1.1/Program.cs
@@ -0,0 +1,67 @@
+﻿void FillArray(int[,] Array)
+{
+    Console.WriteLine("enter start range");
+    int StartRange = int.Parse(Console.ReadLine()!);
+    Console.WriteLine("enter stop range");
+    int StopRange = int.Parse(Console.ReadLine()!);
+    Console.WriteLine();
+
+    int raw = Array.GetLength(0);
+    int column = Array.GetLength(1);
+    for (int i = 0; i < raw; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            Array[i, j] = new Random().Next(StartRange, StopRange);
+        }
+    }
+}
+
+void PrintArray(int[,] SomeArray)
+{
+    int y = SomeArray.GetLength(0);
+    int x = SomeArray.GetLength(1);
+    for (int n = 0; n < y; n++)
+    {
+        for (int m = 0; m < x; m++)
+        {
+            Console.Write(SomeArray[n, m] + "    ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void ChangeArray ( int[,] arr2)
+{
+    int y = arr2.GetLength(0);
+    int x = arr2.GetLength(1);
+    for (int i = 0; i < y; i++)
+    {
+        for (int j = 0; j < x; j++)
+        {
+            if (i == 0 )
+            {
+                (arr2[i,j],arr2[y-1,j]) = (arr2[y-1,j],arr2[i,j]);
+                
+            }
+            Console.Write(arr2[i, j] + "    ");
+        }
+        Console.WriteLine();
+    }
+    
+}
+
+
+
+
+Console.WriteLine("enter rows quantity");
+int raws = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter columns quantity");
+int columns = int.Parse(Console.ReadLine()!);
+int[,] MyArray = new int[raws, columns];
+
+
+FillArray(MyArray);
+PrintArray(MyArray);
+Console.WriteLine();
+ChangeArray(MyArray);
