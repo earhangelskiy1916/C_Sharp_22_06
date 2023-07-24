b91d22d8c9ad321cae2163096c2750dafcc064a2
Evgeniy Arkhangelskiy <earhangelskiy@gmail.com>
2023-07-24 13:10:17 +0300

    feat: add HW 1

---
+++ Lesson9/HW/1.1/Program.cs
@@ -0,0 +1,24 @@
+﻿void EvenNums(int M, int N)
+{
+    if (M >= N)
+        return;
+
+    if (M % 2 == 0)
+    {
+        Console.WriteLine(M);
+        EvenNums(M + 2, N);
+    }
+    else
+    {
+        M = M + 1;
+        Console.WriteLine(M);
+        EvenNums(M + 1, N);
+    }
+}
+
+Console.WriteLine("enter x");
+int x = int.Parse(Console.ReadLine()!);
+Console.WriteLine("enter y");
+int y = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+EvenNums(x, y);
